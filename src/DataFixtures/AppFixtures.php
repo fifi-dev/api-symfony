503224791436b414e40b036016a1ccf67fedea58
Fideline <fideline_bzn@yahoo.com>
2022-12-05 13:58:00 +0100

    fixtures for fake data

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        for ($i = 1; $i <= 10; $i++) {
+            $task = new  Task;
+            $task->setTitle('Ma tâche n° ' . $i);
+            $task->setDescription('Une desciption de la tache ' . $i);
+            $task->setDone(false);
+
+            $manager->persist($task);
+        }
+        $manager->flush();
+    }
+}
